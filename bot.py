30c4d77b7a79ceceacbfaebc44bfe9b6fb23139f
vonssy <kak.reyvaldizakaria@gmail.com>
2025-03-13 18:32:59 +0700

    Update API

--- bot.py
+++ bot.py
@@ -227,11 +227,12 @@ class NaorisProtocol:
                 
                 return self.print_message(self.mask_account(address), proxy, Fore.RED, f"Add to Whitelist Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
     
-    async def toggle_activated(self, address: str, state: str, device_hash: int, proxy=None, retries=5):
-        url = "https://naorisprotocol.network/sec-api/api/toggle"
+    async def toggle_activated(self, address: str, token: str, state: str, device_hash: int, proxy=None, retries=5):
+        url = "https://naorisprotocol.network/sec-api/api/switch"
         data = json.dumps({"walletAddress":address, "state":state, "deviceHash":device_hash})
         headers = {
             **self.headers,
+            "Authorization": f"Bearer {token}",
             "Content-Length": str(len(data)),
             "Content-Type": "application/json"
         }
@@ -247,36 +248,6 @@ class NaorisProtocol:
         
                 return self.print_message(self.mask_account(address), proxy, Fore.RED, f"Turn On Protection Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
     
-    # async def send_heartbeats(self, address: str, device_hash: int, token: str, use_proxy: bool, proxy=None, retries=5):
-    #     url = "https://naorisprotocol.network/sec-api/api/produce-to-kafka"
-    #     data = json.dumps({"topic":"device-heartbeat", "inputData":{"walletAddress":address, "deviceHash":device_hash}})
-    #     headers = {
-    #         **self.headers,
-    #         "Authorization": f"Bearer {token}",
-    #         "Content-Length": str(len(data)),
-    #         "Content-Type": "application/json"
-    #     }
-    #     for attempt in range(retries):
-    #         try:
-    #             response = await asyncio.to_thread(requests.post, url=url, headers=headers, data=data, proxy=proxy, timeout=60, impersonate="safari15_5")
-    #             if response.status_code == 401:
-    #                 token = await self.process_get_access_token(address, use_proxy)
-    #                 headers["Authorization"] = f"Bearer {token}"
-    #                 continue
-
-    #             response.raise_for_status()
-    #             return response.json()
-    #         except Exception as e:
-    #             if attempt < retries - 1:
-    #                 await asyncio.sleep(5)
-    #                 continue
-                
-    #             if "502" in str(e):
-    #                 return self.print_message(self.mask_account(address), proxy, Fore.RED, f"PING Failed: {Fore.YELLOW+Style.BRIGHT}Server Down")
-                
-    #             self.rotate_proxy_for_account(address) if use_proxy else None
-    #             return self.print_message(self.mask_account(address), proxy, Fore.RED, f"PING Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
-            
     async def send_heartbeats(self, address: str, token: str, use_proxy: bool, proxy=None, retries=5):
         url = "https://beat.naorisprotocol.network/api/ping"
         headers = {
@@ -354,9 +325,9 @@ class NaorisProtocol:
             self.print_message(address, proxy, Fore.GREEN, "Add to Whitelist Success")
 
         while True:
-            deactivate = await self.toggle_activated(address, "OFF", device_hash, proxy)
-            if deactivate and deactivate.strip() == "No action needed":
-                activate = await self.toggle_activated(address, "ON", device_hash, proxy)
+            deactivate = await self.toggle_activated(address, token, "OFF", device_hash, proxy)
+            if deactivate and deactivate.strip() in ["Session ended and daily usage updated", "No action needed"]:
+                activate = await self.toggle_activated(address, token, "ON", device_hash, proxy)
                 if activate and activate.strip() == "Session started":
                     self.print_message(address, proxy, Fore.GREEN, "Turn On Protection Success")
                     return True
